ebebaddf1cfe8cd1a457e97f0804de2a7f5d2413
Pamela Hammond <hammondpamelac@gmail.com>
2021-11-03 16:57:41 -0400

    still trying to figure stuff out

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,11 +1,6 @@
 import React from "react";
-import {
-  Jumbotron,
-  Container,
-  CardColumns,
-  Card,
-  Button,
-} from "react-bootstrap";
+import { Jumbotron, Container, Card, Button } from "react-bootstrap";
+import { CardColumns } from "react-bootstrap";
 import { useQuery, useMutation } from "@apollo/react-hooks";
 import { REMOVE_BOOK } from "../utils/mutations";
 import { GET_ME } from "../utils/queries";
